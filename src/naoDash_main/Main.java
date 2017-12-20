3a0bc71d2e5c0211c0c6bbdcbc5c0fd2189d53c6
bachmannmichael <michael.bachmann@netbac.de>
2017-12-20 15:05:18 +0100

    program-Title changed Controller class changed in fxml-file program is executable now ;)

--- src/naoDash_main/Main.java
+++ src/naoDash_main/Main.java
@@ -10,11 +10,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("main_scene.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
-        System.out.println("Hello World");
+
     }
 
 
