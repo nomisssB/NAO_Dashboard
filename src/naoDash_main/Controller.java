a6baf4d11221201f5edb11199119982cab525496
bachmannmichael <michael.bachmann@netbac.de>
2017-12-22 10:25:36 +0100

    ##### CONTROLLER - added sample Key-Event for "W" Button - added Logger
    ##### main_scene.fxml
    - added stylesheet (style_main_scene.css)

--- src/naoDash_main/Controller.java
+++ src/naoDash_main/Controller.java
@@ -3,18 +3,26 @@ package naoDash_main;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 
 public class Controller {
     public double motionspeed = 0;
     @FXML
+    public VBox vbox_main;
+    @FXML
     public Slider sldr_speed;
     @FXML
     public Button btn_s;
     @FXML
-    private Button btn_w;
+    public Button btn_w;
     @FXML
-    private Label lbl_mid;
+    public Label lbl_mid;
 
 
     public Controller(){
@@ -23,29 +31,41 @@ public class Controller {
     }
 
     @FXML
-    public void initialize(){
+    public void initialize() throws IOException {
+        //Logger um Events und Fehler zu dokumentieren. Log Datei unter root\main_log.log (XML)
+        FileHandler handler = new FileHandler("main_log.log", true);
+        Logger logger = Logger.getLogger("NAODash Logger");
+        logger.addHandler(handler);
+
         sldr_speed.valueProperty().addListener((observable, oldValue, newValue) -> {
             lbl_mid.setText("value: " + newValue.intValue());
+
         });
+
+
+
+        //Abfangen von KeyEvents
+        vbox_main.setOnKeyPressed(e ->{
+            if(e.getCode() == KeyCode.W) {
+                logger.info("Button W pressed and fired");
+                btn_w.fire();
+            }
+        });
+
     }
 
 
 
     public void forward(ActionEvent actionEvent){
         lbl_mid.setText("forward");
+
     }
 
     public void left(ActionEvent actionEvent) {
         lbl_mid.setText("left");
 
     }
-
-    public void setSpeed(ActionEvent actionEvent) {
-
-
-
-
-    }
+    
 }
 
 
--- src/naoDash_main/Main.java
+++ src/naoDash_main/Main.java
@@ -1,22 +1,22 @@
 package naoDash_main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
 
+public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
+        //Erzeugung der Main-Scene "root" aus main_scene.fxml
+
         Parent root = FXMLLoader.load(getClass().getResource("main_scene.fxml"));
-
-
+        primaryStage.setTitle("NaoDashboard MSKM");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
     }
 
 
